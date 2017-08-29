9232050ed25e094570beff2a75fa8641f7ac7339
komar <komar@irito.ru>
2017-08-29 16:24:07 +0300

    rename SQLstrconv -> SQLiteStrconv

--- utils.go
+++ utils.go
@@ -1,6 +1,6 @@
 package simpledb
 
-//SQLstrconv - converting interface{}(SQL string) to string
-func SQLstrconv(input interface{}) string {
+//SQLiteStrconv - converting interface{}(SQL string) to string
+func SQLiteStrconv(input interface{}) string {
 	return string(input.([]uint8))
 }
