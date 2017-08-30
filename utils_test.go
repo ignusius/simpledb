0d45923ee4cab3fed4b59f491fc91e5ae1ebfb52
komar <komar@irito.ru>
2017-08-30 16:50:50 +0300

    Add test for utils.go

---
+++ utils_test.go
@@ -0,0 +1,14 @@
+package simpledb
+
+import "testing"
+
+func TestSQLiteStrconv(t *testing.T) {
+	expected := "test"
+	var input interface{}
+	input = []uint8{116, 101, 115, 116}
+	res := SQLiteStrconv(input)
+
+	if res != expected {
+		t.Fatal("Warring! value != expected")
+	}
+}
