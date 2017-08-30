dd31887afa94ad0ad166c6a5001a9d75f079032b
komar <komar@irito.ru>
2017-08-30 13:33:51 +0300

    Add DB field from work with database/sql directly

--- db.go
+++ db.go
@@ -1,23 +1,30 @@
-//Package simpledb for simple use databases.
+//Package simpleDb for simple use databases.
 package simpledb
 
 import (
+	"fmt"
 	"database/sql"
 	"errors"
 )
 
 //DB is structure of database
 type DB struct {
-	db   *sql.DB
+	Db   *sql.DB
 	err  error
 	rows *sql.Rows
 }
 
 //NewDatabase - create new database
 func (d *DB) NewDatabase(database, basename string) error {
-	d.db, d.err = sql.Open(database, basename)
+	d.Db, d.err = sql.Open(database, basename)
 	if d.err != nil {
+		fmt.Println(d.err)
 		return errors.New("DataBase not found")
+	}
+	d.err = d.Db.Ping()
+	if d.err != nil {
+		fmt.Println(d.err)
+		return errors.New("Connection failed")
 	}
 	return nil
 }
@@ -25,11 +32,12 @@ func (d *DB) NewDatabase(database, basename string) error {
 //Query - for query from database
 func (d *DB) Query(query string, args ...interface{}) ([][]interface{}, error) {
 	var emptyReturn [][]interface{}
-
-	d.rows, d.err = d.db.Query(query, args...)
+	d.rows, d.err = d.Db.Query(query, args...)
 	if d.err != nil {
+		fmt.Println(d.err)
 		return emptyReturn, errors.New("Query -> query error")
 	}
+	defer d.rows.Close()
 	var varReturn [][]interface{}
 
 	col, _ := d.rows.Columns()
@@ -44,6 +52,7 @@ func (d *DB) Query(query string, args ...interface{}) ([][]interface{}, error) {
 		}
 		d.err = d.rows.Scan(varArrPtrs...)
 		if d.err != nil {
+			fmt.Println(d.err)
 			return emptyReturn, errors.New("Query -> row.Scan error")
 		}
 		varReturn = append(varReturn, tmp)
@@ -63,16 +72,15 @@ func (d *DB) Query(query string, args ...interface{}) ([][]interface{}, error) {
 		counter++
 	}
 
-	d.rows.Close()
-
 	return varReturn, nil
 
 }
 
 //Exec - for exec to database
 func (d *DB) Exec(exec string, args ...interface{}) error {
-	_, d.err = d.db.Exec(exec, args...)
+	_, d.err = d.Db.Exec(exec, args...)
 	if d.err != nil {
+		fmt.Println(d.err)
 		return d.err
 	}
 	return nil
@@ -80,5 +88,15 @@ func (d *DB) Exec(exec string, args ...interface{}) error {
 
 //Close - close  database
 func (d *DB) Close() {
-	d.db.Close()
+	d.Db.Close()
+}
+
+//Ping - chack status connection to database
+func (d *DB) Ping() error {
+	d.err = d.Db.Ping()
+	if d.err != nil {
+		fmt.Println(d.err)
+		return d.err
+	}
+	return nil
 }
