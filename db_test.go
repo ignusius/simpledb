36e3f83b94f9778dabe55e48fb861f433c7a96f1
komar <komar@irito.ru>
2017-09-04 22:20:11 +0300

    Add new tests, but many test work wrong

--- db_test.go
+++ db_test.go
@@ -1,6 +1,7 @@
 package simpledb
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
@@ -33,12 +34,95 @@ func TestQuery(t *testing.T) {
 	expected := []interface{}{1, "test", "test", 2, 3}
 	if reflect.DeepEqual(expected, arr[0]) {
 		t.Fatal("Warring! value != expected")
-		
+
 	}
 	err = db.Exec("DELETE FROM DATA")
 	if err != nil {
 		t.Fatal("Error! Clearing database error")
 	}
-	db.Close()
+	//db.Close()
 
+}
+
+func TestPrepare(t *testing.T) {
+
+	err := db.Prepare("INSERT INTO data values ($1,$2,$3,$4,$5)")
+	if err != nil {
+		t.Fatal("Error! Error Prepare!")
+	}
+	err = db.StmtExec(1, "test", "test", 2, 3)
+	if err != nil {
+		t.Fatal("Error! Error Stmt!")
+	}
+	arr, err := db.Query("SELECT * FROM data")
+	if err != nil {
+		t.Fatal("Error! Error Query!")
+	}
+	expected := []interface{}{1, "test", "test", 2, 3}
+	if !compare(expected, arr[0]) {
+		t.Fatal("Warring! value != expected")
+
+	}
+	err = db.Exec("DELETE FROM DATA")
+	if err != nil {
+		t.Fatal("Error! Clearing database error")
+	}
+
+}
+
+func TestTxPrepare(t *testing.T) {
+
+	db.Begin()
+
+	err := db.TxPrepare("INSERT INTO data values ($1,$2,$3,$4,$5)")
+	if err != nil {
+		t.Fatal("Error! Error Prepare!")
+	}
+	err = db.StmtExec(1, "test", "test", 2, 3)
+	if err != nil {
+		t.Fatal("Error! Error Stmt!")
+	}
+	db.Commit()
+	arr, err := db.Query("SELECT * FROM data")
+	if err != nil {
+		t.Fatal("Error! Error Query!")
+	}
+	expected := []interface{}{1, "test", "test", 2, 3}
+	if !reflect.DeepEqual(expected, arr[0]) {
+		t.Fatal("Warring! value != expected")
+
+	}
+	//err = db.Exec("DELETE FROM DATA")
+	///if err != nil {
+	//	t.Fatal("Error! Clearing database error")
+	//}
+
+}
+
+func TestClose(t *testing.T) {
+	db.Close()
+}
+
+func compare(a, b []interface{}) bool {
+	fmt.Println(reflect.TypeOf(a[0]))
+	fmt.Println(reflect.TypeOf(b[0]))
+	fmt.Println(reflect.DeepEqual(a, b))
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if reflect.TypeOf(a[i]) == reflect.Int64 {
+
+			if int64(a[i]) != int64(b[i]) {
+				return false
+
+			}
+
+		} else {
+			a[i] != b[i]
+			return false
+
+		}
+		return true
+	}
 }
